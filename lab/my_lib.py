882baa9845f9e31e496b935e82056319e450b9a2
Risonans <slavca123@yandex.ru>
2024-10-16 20:50:50 +0500

    init project

---
+++ lab/my_lib.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# Функция сортировки пузырьком по возрастанию и убыванию
+# Принимает mas, x
+# Возвращает список из чисел
+# Пример: [3, 2, 1, 7], 1 -> [1, 2, 3, 7]; [3, 2, 1, 7], 2 -> [7, 3, 2, 1]
+# Классы эквивалентности: массивы, буквы, цифры, специальные символы
+# Граничные значения по int
+def bubble(mas, x):
+    if mas == []: return 0
+    if x == 1:
+        for i in range(len(mas) - 1):
+            for j in range(len(mas) - 1 - i):
+                if mas[j] > mas[j + 1]:
+                    mas[j], mas[j + 1] = mas[j + 1], mas[j]
+        return mas
+    if x == 2:
+        for i in range(len(mas) - 1):
+            for j in range(len(mas) - 1 - i):
+                if mas[j] < mas[j + 1]:
+                    mas[j], mas[j + 1] = mas[j + 1], mas[j]
+        return mas
